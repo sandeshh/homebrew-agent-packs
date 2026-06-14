8d604f4a8fc5b3b2e7b703f21d0848b275067b42
Sandesh Hegde <sandesh.hegde@gmail.com>
2026-06-14 12:45:49 -0700

    Fix agent-packs v0.1.0 checksums

--- Formula/agent-packs.rb
+++ Formula/agent-packs.rb
@@ -6,21 +6,21 @@ class AgentPacks < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sandeshh/agent-packs/releases/download/v#{version}/agent-packs_darwin_arm64.tar.gz"
-      sha256 "aa0a984bfb7474991daadf992806b4cb6833640820be05dffbdb1d980596cf4a"
+      url "https://github.com/sandeshh/agent-packs/releases/download/v0.1.0/agent-packs_darwin_arm64.tar.gz"
+      sha256 "e4db9b808f223e42de72f7e01a35ed6b2d52a953d133b74ac28955cee9438c27"
     else
-      url "https://github.com/sandeshh/agent-packs/releases/download/v#{version}/agent-packs_darwin_amd64.tar.gz"
-      sha256 "2cccec5cbe9f2c357235aa95c41d9b942049700115f9eabc5aac415f6ae0bdbe"
+      url "https://github.com/sandeshh/agent-packs/releases/download/v0.1.0/agent-packs_darwin_amd64.tar.gz"
+      sha256 "25c46e2a188ca0b529669539b5c875f240cbbd5757df813c918bbed6f62a017a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sandeshh/agent-packs/releases/download/v#{version}/agent-packs_linux_arm64.tar.gz"
-      sha256 "70cc64a7a8d75d179dd38844abed3b147f27e9649bd47414264f93baeeb0a630"
+      url "https://github.com/sandeshh/agent-packs/releases/download/v0.1.0/agent-packs_linux_arm64.tar.gz"
+      sha256 "0f929cd105be3eedac5ebe667ea4aba71bbf85f72931e58d975fea955122170a"
     else
-      url "https://github.com/sandeshh/agent-packs/releases/download/v#{version}/agent-packs_linux_amd64.tar.gz"
-      sha256 "bec591d15d1ee726a9fdb239c95aee1dc6fab81c2b668169bec78cf5f4237a2f"
+      url "https://github.com/sandeshh/agent-packs/releases/download/v0.1.0/agent-packs_linux_amd64.tar.gz"
+      sha256 "4d68dabfa3e73b9573bc2683e9da51c5887881906077edd4ef22921484b729c4"
     end
   end
 
@@ -41,6 +41,6 @@ class AgentPacks < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/agent-packs version")
+    assert_match version.to_s, shell_output("\#{bin}/agent-packs version")
   end
 end
